e75840d2ca7e5bde0453514438b82fcbdb42727c
Miguel Ángel Barquilla <miguelangelbarquilla@gmail.com>
2022-10-07 22:22:17 +0200

    Fix sintax in file .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
-  rules: {},
-  "import/extensions": ["error", "always"],
+  rules: {
+    "import/extensions": ["error", "always"],
+  },
 };
