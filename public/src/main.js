78bbe181b49ec8fb32cb46aa9fd1d06d18a5dd04
Miguel Ángel Barquilla <46623652+Mibarfu@users.noreply.github.com>
2022-10-09 11:07:34 +0200

    Implement the BuilderGrid Class and initial styles are given

--- public/src/main.js
+++ public/src/main.js
@@ -1,8 +1,21 @@
 import WordMaker from "./WordMaker/WordMaker.js";
+import BuilderGrid from "./BuiderGrid/BuilderGrid.js";
 
 const rows = 100;
 const columns = 100;
+const sizCell = 3;
 
+const gridElement = document.querySelector(".grid-canvas");
 const word = new WordMaker(rows, columns);
 
-console.log(word);
+word.randonLife();
+
+const grid = new BuilderGrid(
+  rows,
+  columns,
+  sizCell,
+  gridElement,
+  word.matrixWord
+);
+
+grid.buildGrid();
