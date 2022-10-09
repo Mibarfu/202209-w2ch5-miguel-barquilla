a7ed13cac5fc7a5aecb48169aa5cec6b498c1cf7
Miguel Ángel Barquilla <46623652+Mibarfu@users.noreply.github.com>
2022-10-09 21:14:37 +0200

    Add functionality to play button

--- public/src/eventsBottons/eventsButtons.js
+++ public/src/eventsBottons/eventsButtons.js
@@ -1,4 +1,7 @@
-const declareEventsRestartButton = (word, grid) => {
+import nextGeneration from "../nextGeneration/nextGeneration.js";
+import nextToCurrentGeneration from "../nextToCurrentGeneration/nextToCurrentGeneration.js";
+
+export const declareEventsRestartButton = (word, grid) => {
   document.querySelector(".restart-button").addEventListener("click", () => {
     word.randonLife();
 
@@ -6,4 +9,30 @@ const declareEventsRestartButton = (word, grid) => {
   });
 };
 
-export default declareEventsRestartButton;
+export const declareEventsPlayButton = (rows, columns, word, grid) => {
+  document.querySelector(".play-button").addEventListener("click", () => {
+    setInterval(() => {
+      const word2 = word;
+      const nextGenerationArray = nextGeneration(
+        rows,
+        columns,
+        word.matrixWord
+      );
+      grid.buildGrid(nextGenerationArray);
+      word2.matrixWord = nextToCurrentGeneration(
+        rows,
+        columns,
+        nextGenerationArray
+      );
+    }, 100);
+  });
+};
+
+// export const declareEventsStopButton = (word, grid) => {
+//   document.querySelector(".restart-button").addEventListener("click", () => {
+//     word.randonLife();
+
+//     grid.buildGrid(word.matrixWord);
+//   });
+// };
+// declareEventsRestartButton;
--- public/src/main.js
+++ public/src/main.js
@@ -1,8 +1,9 @@
 import WordMaker from "./WordMaker/WordMaker.js";
 import BuilderGrid from "./BuiderGrid/BuilderGrid.js";
-import nextGeneration from "./nextGeneration/nextGeneration.js";
-import nextToCurrentGeneration from "./nextToCurrentGeneration/nextToCurrentGeneration.js";
-import declareEventsRestartBotton from "./eventsBottons/eventsButtons.js";
+import {
+  declareEventsRestartButton,
+  declareEventsPlayButton,
+} from "./eventsBottons/eventsButtons.js";
 
 const columns = 300;
 const rows = 150;
@@ -17,12 +18,5 @@ const grid = new BuilderGrid(rows, columns, sizCell, gridElement);
 
 grid.buildGrid(word.matrixWord);
 
-declareEventsRestartBotton(word, grid);
-
-// setInterval(() => {
-//   const nextGenerationArray = nextGeneration(rows, columns, word.matrixWord);
-
-//   grid.buildGrid(nextGenerationArray);
-
-//   word.matrixWord = nextToCurrentGeneration(rows, columns, nextGenerationArray);
-// }, 100);
+declareEventsRestartButton(word, grid);
+declareEventsPlayButton(rows, columns, word, grid);
